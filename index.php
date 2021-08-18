2033a7d3acb113ef18f7c65a2e3e89d3dd5770a3
Punyisaa <88873965+Punyisaa@users.noreply.github.com>
2021-08-19 00:37:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <h1>Punyisa Kaewchuen<h1>
 
-<iframe src = https://thingspeak.com/channels/1464734/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15  style="height:40%;width:35%;"></iframe>
+<iframe src = https://thingspeak.com/channels/1464734/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15 style="height:40%;width:35%;"></iframe>
   <br></br>
- <iframe src =https://thingspeak.com/channels/1464734/charts/4?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15  style="height:40%;width:35%;"></iframe>
+ <iframe src =https://thingspeak.com/channels/1464734/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15  style="height:40%;width:35%;"></iframe>
 <br></br>
   <iframe src =https://thingspeak.com/channels/1464734/maps/channel_show  style="height:45%;width:35%;"></iframe>
 <html>
