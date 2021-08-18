0a9f0d401f0d7e600655b70332893612f18ea7aa
Punyisaa <88873965+Punyisaa@users.noreply.github.com>
2021-08-18 23:52:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-Punyisa Kaewchuen
+<h1>Punyisa Kaewchuen<h1>
+
+<iframe src = https://thingspeak.com/channels/1464734/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15  style="height:40%;width:35%;"></iframe>
+  <br></br>
+ <iframe src =https://thingspeak.com/channels/1464734/charts/4?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15  style="height:40%;width:35%;"></iframe>
+<br></br>
+  <iframe src =https://thingspeak.com/channels/1464734/maps/channel_show  style="height:45%;width:35%;"></iframe>
+<html>
+<head>
+<script type="text/JavaScript">
+
+function timedRefresh(timeoutPeriod) {
+    setTimeout("location.reload(true);",timeoutPeriod);
+}
+
+</script>
+</head>
+<body onload="JavaScript:timedRefresh(15000);">
+
+<?php 
+ $humidity = file_get_contentshttps('http://api.thingspeak.com/channels/1464734/feeds.json?results=1');
+$temp = file_get_contentshttps('http://api.thingspeak.com/channels/1464734/feeds.json?results=1');
+
+ echo "<br> temp is = ".$temp."<br>";
+ echo "humidity is = ".$humidity;
+ 
+?>
+
+</body>
+</html>
